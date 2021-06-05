dd6e6689242810a2d7d7e18d2a4d489f33b70eb8
SathwikChinnu <85409798+SathwikChinnu@users.noreply.github.com>
2021-06-05 23:21:32 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+function Note() {
+  return (
+    <div className="note">
+      <h2>JavaScript and Reactjs</h2>
+      <h1>7 days free Bootcamp</h1>
+      <p>
+        I'm Sai Sathwik pursuing Btech 2nd Year. This was an amazing Bootcamp
+        which was taken up by Mr.Shaurya Sinha. I learnt basics on Reactjs and
+        Javascript from scratch. We created it by using codesandbox.io.
+      </p>
+      <p>Thanks ShapeAI...</p>
+    </div>
+  );
+}
+export default Note;
